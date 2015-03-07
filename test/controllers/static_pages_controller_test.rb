600abba64ee79634d7ee9d319c8f7da86936f8a3
Zrrg <hiroaritillwhen@gmail.com>
2015-03-07 15:14:17 +0300

    Add pathes and links

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,14 +4,14 @@ class StaticPagesControllerTest < ActionController::TestCase
 
 
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = "Not so epic place"
   end
 
   test "should get home" do
     get :home
     assert_response :success
 
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
@@ -19,11 +19,15 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
-
-   test "should get about" do
+     test "should get about" do
     get :about
     assert_response :success
     assert_select "title", "About | #{@base_title}"
+  end
+   test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
